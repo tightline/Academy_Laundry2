7817b0be77837c04957b673a430b59fa2a712aae
tightline <rjbyrne15@gmail.com>
2018-02-21 16:07:40 -0500

    Update AppConfig.java

--- app/src/main/java/ryan/academy_laundry2/app/AppConfig.java
+++ app/src/main/java/ryan/academy_laundry2/app/AppConfig.java
@@ -2,20 +2,20 @@ package ryan.academy_laundry2.app;
 
 public class AppConfig {
 	// Server user login url
-	public static String URL_LOGIN = "https://myrasppi.hopto.org/academy/login.php";
+	public static String URL_LOGIN = "https://";
 
 	// Server user register url
-	public static String URL_REGISTER = "https://myrasppi.hopto.org/academy/register.php";
+	public static String URL_REGISTER = "https://";
 
 	//Server to enter weight
-	public static String URL_STORE_WEIGHT ="https://myrasppi.hopto.org/academy/store_weights.php";
+	public static String URL_STORE_WEIGHT ="https://";
 
 	//Server to enter weight
-	public static String URL_GET_WEIGHT ="https://myrasppi.hopto.org/academy/get_weights.php";
+	public static String URL_GET_WEIGHT ="https://";
 
 
-	public static String URL_GET_CUSTOMERS ="https://myrasppi.hopto.org/academy/get_customers.php";
+	public static String URL_GET_CUSTOMERS ="https://";
 
-	public static String URL_STORE_COUNTS ="https://myrasppi.hopto.org/academy/store_counts.php";
+	public static String URL_STORE_COUNTS ="https://";
 
 }
